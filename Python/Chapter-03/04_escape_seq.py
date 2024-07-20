173e40e230970a021fdca6126636ce09ff999e80
ANISH KANRAR <147317580+Anish-Kanrar@users.noreply.github.com>
2024-07-20 23:40:42 +0530

    Update 04_escape_seq.py

--- Python/Chapter-03/04_escape_seq.py
+++ Python/Chapter-03/04_escape_seq.py
@@ -1,1 +1,20 @@
-a = "Anik is a good boy but a bad boy"
+#Escape sequence characters
+# In Python, escape sequence characters are used to represent certain special characters within a string. They are preceded by a backslash ("\"). Here are some commonly used escape sequences in Python:
+'''
+\\ : Backslash (\)
+\' : Single quote (')
+\" : Double quote (")
+\n : Newline
+\t : Horizontal tab
+\r : Carriage return
+\b : Backspace
+\f : Form feed
+\v : Vertical tab
+\a : Bell/alert
+'''
+
+a = "Anik is a good boy\nbut a bad\tboy"
+b = "Animesh is a good boy but a bad \"boy\""
+
+print(a)
+print(b)
